c78bc4516ecdc6e5d4e6c0741d413abd14ec246d
Maksym <maks@iMac-Maksym.fritz.box>
2023-07-13 18:35:01 +0200

    add tv show page

---
+++ src/js/partials/renderingalltvshow.js
@@ -0,0 +1,50 @@
+export function renderingAllTvShowList(response) {
+  const arr = response.results;
+  const movieList = document.querySelector('.all-movies-list');
+
+  const movieListMarkup = arr
+    .map(({ poster_path, title, release_date, vote_average }) => {
+      const markup = `
+    <li>
+		<div class="movie-card">
+			<a href="movie-details.html" target="_blank">
+			<figure class="card-banner">
+				<img
+				src="https://image.tmdb.org/t/p/w300${poster_path}"
+				alt="${title}"
+				/>
+			</figure>
+			</a>
+
+			<div class="title-wrapper">
+			<a href="movie-details.html" target="_blank">
+				<h3 class="card-title">${title}</h3>
+			</a>
+
+			<time datetime="${release_date}">${release_date}</time>
+			</div>
+
+			<div class="card-meta">
+			<div class="badge badge-outline">HD</div>
+
+			<div class="duration">
+				<ion-icon name="time-outline"></ion-icon>
+
+				<time datetime="PT137M">137 min</time>
+			</div>
+
+			<div class="rating">
+				<ion-icon name="star"></ion-icon>
+
+				<data>${vote_average.toFixed(1)}</data>
+			</div>
+			</div>
+		</div>
+	</li>`;
+
+      return markup;
+    })
+    .join('');
+
+  movieList.insertAdjacentHTML('beforeend', movieListMarkup);
+}
---
+++ src/js/partials/tvshow-list.js
@@ -0,0 +1,18 @@
+import '../partials/header';
+import NewApiService from '../get-api';
+import { renderingAllTvShowList } from '../partials/renderingalltvshow';
+
+const newApiService = new NewApiService();
+
+// Rendering movies list
+newApiService.getAllTvShow().then(renderingAllTvShowList);
+
+// Show More Button
+const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+
+// Rendering more movies on click Show More Btn
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onLoadMore() {
+  newApiService.getAllMoviesList().then(renderingAllTvShowList);
+}
