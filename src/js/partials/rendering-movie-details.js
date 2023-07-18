c18431e16596cc3b55b31357785cfa0368ca523a
Maksym <maks@iMac-Maksym.fritz.box>
2023-07-18 17:12:45 +0200

    add movie details

--- src/js/partials/rendering-movie-details.js
+++ src/js/partials/rendering-movie-details.js
@@ -1,88 +1,114 @@
-import NewApiService from '../get-api';
+// import NewApiService from '../get-api';
 
-const newApiService = new NewApiService();
+// const newApiService = new NewApiService();
 
 const savedMovieInfo = JSON.parse(localStorage.getItem('current-movie-info'));
 
 console.log(savedMovieInfo);
+{
+  /* <div class="badge badge-fill">PG 13</div>; */
+}
+function renderingMovieDetails(data) {
+  const detailsWrapper = document.querySelector('#details-wrapper');
+  const {
+    poster_path,
+    original_title,
+    overview,
+    title,
+    name,
+    genres,
+    runtime,
+    release_date,
+    status,
+    popularity,
+    production_countries,
+  } = data;
+  const genreLinks = genres
+    .map(genre => `<a href="#">${genre.name}</a>`)
+    .join('');
 
-function renderingMovieDetails() {
-  //
+  const prodcutionCountries = production_countries
+    .map(country => `<p class="countries">${country.name}</p>`)
+    .join('');
+
+  const movieInfoMarkup = `
+	<figure class="movie-detail-banner">
+      <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${title}" width="300"/>
+
+      <button class="play-btn">
+        <ion-icon name="play-circle-outline"></ion-icon>
+      </button>
+    </figure>
+
+    <div class="movie-detail-content">
+      <p class="detail-subtitle">${status}</p>
+
+      <h1 class="h1 detail-title">${title || original_title || name}</h1>
+
+      <div class="meta-wrapper">
+        <div class="badge-wrapper">
+          
+
+          <div class="badge badge-outline">HD</div>
+        </div>
+
+        <div class="ganre-wrapper">
+          ${genreLinks}
+        </div>
+
+        <div class="date-time">
+          <div>
+            <ion-icon name="calendar-outline"></ion-icon>
+
+            <time datetime="${release_date}">${release_date}</time>
+          </div>
+
+          <div>
+            <ion-icon name="time-outline"></ion-icon>
+
+            <time datetime="PT115M">${runtime} min</time>
+          </div>
+		  <div class="badge">Popularity: ${popularity.toFixed(1)}</div>;
+        </div>
+      </div>
+
+      <p class="storyline">
+        ${overview}
+      </p>
+	  <div class="production-countries">
+	 	 <p >Production Countries: ${prodcutionCountries}</p>
+	  </div>
+	  
+	  
+
+      <div class="details-actions">
+        <button class="share">
+          <ion-icon name="share-social"></ion-icon>
+
+          <span>Share</span>
+        </button>
+
+        <div class="title-wrapper">
+          <p class="title">Prime Video</p>
+
+          <p class="text">Streaming Channels</p>
+        </div>
+
+        <button class="btn btn-primary">
+          <ion-icon name="play"></ion-icon>
+
+          <span>Watch Now</span>
+        </button>
+      </div>
+
+      <a href="https://image.tmdb.org/t/p/w300${poster_path}" download class="download-btn">
+        <span>Download</span>
+
+        <ion-icon name="download-outline"></ion-icon>
+      </a>
+    </div>`;
+
+  detailsWrapper.innerHTML = movieInfoMarkup;
 }
 
-// `<figure class="movie-detail-banner">
-//       <img src="/src/img/top-rated/movie-4.png" alt="Free guy movie poster" />
-
-//       <button class="play-btn">
-//         <ion-icon name="play-circle-outline"></ion-icon>
-//       </button>
-//     </figure>
-
-//     <div class="movie-detail-content">
-//       <p class="detail-subtitle">New Episodes</p>
-
-//       <h1 class="h1 detail-title">Free <strong>Guy</strong></h1>
-
-//       <div class="meta-wrapper">
-//         <div class="badge-wrapper">
-//           <div class="badge badge-fill">PG 13</div>
-
-//           <div class="badge badge-outline">HD</div>
-//         </div>
-
-//         <div class="ganre-wrapper">
-//           <a href="#">Comedy,</a>
-
-//           <a href="#">Action,</a>
-
-//           <a href="#">Adventure,</a>
-
-//           <a href="#">Science Fiction</a>
-//         </div>
-
-//         <div class="date-time">
-//           <div>
-//             <ion-icon name="calendar-outline"></ion-icon>
-
-//             <time datetime="2021">2021</time>
-//           </div>
-
-//           <div>
-//             <ion-icon name="time-outline"></ion-icon>
-
-//             <time datetime="PT115M">115 min</time>
-//           </div>
-//         </div>
-//       </div>
-
-//       <p class="storyline">
-//         A bank teller called Guy realizes he is a background character in an
-//         open world video game called Free City that will soon go offline.
-//       </p>
-
-//       <div class="details-actions">
-//         <button class="share">
-//           <ion-icon name="share-social"></ion-icon>
-
-//           <span>Share</span>
-//         </button>
-
-//         <div class="title-wrapper">
-//           <p class="title">Prime Video</p>
-
-//           <p class="text">Streaming Channels</p>
-//         </div>
-
-//         <button class="btn btn-primary">
-//           <ion-icon name="play"></ion-icon>
-
-//           <span>Watch Now</span>
-//         </button>
-//       </div>
-
-//       <a href="/src/img/top-rated/movie-4.png" download class="download-btn">
-//         <span>Download</span>
-
-//         <ion-icon name="download-outline"></ion-icon>
-//       </a>
-//     </div>`;
+renderingMovieDetails(savedMovieInfo);
